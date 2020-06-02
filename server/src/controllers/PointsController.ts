2d7fd8ae012edd877a2eb6844261c68e3bb27f65
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-06-02 12:44:47 -0300

    Rotas finalizadas

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -3,7 +3,12 @@ import { Request, Response } from 'express'
 
 class PointsController {
     async index(request: Request, response: Response){
-        
+        const { city, uf, items } = request.query
+        const parsedItems = String(items).split(',').map(item => Number(item.trim()))
+
+        const points = await knex('points').join('point_items', 'points.id', '=', 'point_items.point_id').whereIn('point_items.item_id', parsedItems).where('city', String(city)).where('uf', String(uf)).distinct().select('points.*')
+
+        return response.json(points)
     }
     async show(request: Request, response: Response){
         const { id } = request.params
