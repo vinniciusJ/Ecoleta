f77543d78c39e4a9564024bfa130b0c5c1eef4d9
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-06-03 16:05:52 -0300

    Conceitos React

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Header from './Header'
+
+// JSX: Sintaxe de XML dentro do JavaScript
 
 function App() {
+  const [counter, setCounter] = useState(0)  // Retorna um array => [value, function to update]
+
+  function handleButtonClick(){
+    setCounter(counter + 1)
+  }
+  
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Header title="Hello World"/>
+
+      <h2>{counter}</h2>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
   );
 }
 
