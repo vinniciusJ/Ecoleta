a75571f1cb8dd59cf8977221c8ed9f8f91039776
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-06-04 22:39:32 -0300

    Iniciando a Point screen

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import { View, Image, StyleSheet } from 'react-native'
-
+import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native'
+import { Feather as Icon} from '@expo/vector-icons'
+import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
+    const navigation = useNavigation()
+
+    function handleNavigationToPoints(){
+      navigation.navigate('Points')
+    }
+
     return (
-        <View style={styles.container}>
-            <Image source={require('../../assets/logo.png')} />
-        </View>
+        <ImageBackground source={require('../../assets/home-background.png')} imageStyle={{width: 274, height: 368}} style={styles.container} >
+            <View style={styles.main}>
+                <Image source={require('../../assets/logo.png')} />
+                <Text style={styles.title}>Seu marketplace de coleta de resíduos.</Text>
+                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+            </View>
+
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={handleNavigationToPoints}>
+                    <View style={styles.buttonIcon}>
+                        <Icon name="arrow-right" color="#FFF" size={24}/>
+                    </View>
+                    <Text style={styles.buttonText}>Entrar</Text>
+                </RectButton>            
+            </View>
+        </ImageBackground>
     )
 }
 
