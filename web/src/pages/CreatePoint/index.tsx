2b12724b76e0662edff135ed5b5a0f1ace7c78cc
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-06-05 22:13:16 -0300

    Upload de imagem

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react'
+import Dropzone from '../../components/Dropzone/'
 import { Link, useHistory } from 'react-router-dom'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import { LeafletMouseEvent } from 'leaflet'
@@ -40,6 +41,7 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState('0')
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0])
     const [selectedItems, setSelectedItems] = useState<number[]>([])
+    const [selectedFile, setSelectedFile] = useState<File>()
 
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0])
 
@@ -125,17 +127,20 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition
         const items = selectedItems
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        }
+        const data = new FormData()
 
+        data.append('name', name)
+        data.append('email', email)
+        data.append('whatsapp', whatsapp)
+        data.append('uf', uf)
+        data.append('city', city)
+        data.append('latitude', String(latitude))
+        data.append('longitude', String(longitude))
+        data.append('items', items.join(','))
+        
+        if(selectedFile){
+            data.append('image', selectedFile)
+        }
         await api.post('points', data)
 
         alert('Ponto de coleta criado')
@@ -154,6 +159,8 @@ const CreatePoint = () => {
             </header>
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br/> ponto de Coleta</h1>
+                
+                <Dropzone onFileUploaded={setSelectedFile}/>
                 <fieldset>
                     <legend>
                         <h2>Dados</h2>
